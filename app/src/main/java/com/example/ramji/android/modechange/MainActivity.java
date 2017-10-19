0a0c4d8ffa02d7454ea17d72e828fc6d8105c10d
Ramji <ramji.b28@gmail.com>
2017-10-19 10:00:20 +0530

    Setting up Google API Client and granting location permissions

--- app/src/main/java/com/example/ramji/android/modechange/MainActivity.java
+++ app/src/main/java/com/example/ramji/android/modechange/MainActivity.java
@@ -1,17 +1,38 @@
 package com.example.ramji.android.modechange;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     private static final String TAG = "MainActivity";
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 111;
 
     // Member variables
     private PlaceListAdapter mAdapter;
     private RecyclerView mRecyclerView;
+    private CheckBox locationPermissionCB;
+    private Button addButton;
 
     /**
      * Called when the activity is starting
@@ -29,5 +50,67 @@ public class MainActivity extends AppCompatActivity {
 
         mAdapter = new PlaceListAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
+
+        //Creating GoogleApiClient with the location services API and GEO_DATA_API
+
+        GoogleApiClient client = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
+                .enableAutoManage(this,this)
+                .build();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        Log.i(TAG,"API Client Connection Successful");
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+        Log.i(TAG,"API Client Connection Suspended");
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG,"API Client Connection Failed");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Initiliasing location permission checkbox
+
+        locationPermissionCB = (CheckBox) findViewById(R.id.location_permission_checkbox);
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            locationPermissionCB.setChecked(false);
+        }else{
+            locationPermissionCB.setChecked(true);
+            locationPermissionCB.setEnabled(false);
+        }
+
+    }
+
+    public void onAddPlaceButtonClicked(View view) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, getString(R.string.need_location_permission_message), Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(this, getString(R.string.location_permissions_granted_message), Toast.LENGTH_SHORT).show();
+    }
+
+    public void onLocationPermissionClicked(View view) {
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSIONS_REQUEST_FINE_LOCATION);
+
     }
 }
